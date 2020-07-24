f88ea2aaaaa8f9b8a014d3fe12b978fc1f2004b0
Cody Fritz <clanalia55@gmail.com>
2020-07-24 11:40:01 -0700

    add support and basic functionality in to all MVC files and controllers

---
+++ PierresWebsite/Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresWebsite.Models;
+using System.Collections.Generic;
+
+namespace PierresWebsite.Controllers
+{
+  public class OrdersController : Controller
+  {
+    
+  }
+}
---
+++ PierresWebsite/Controllers/VendorsController.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PierresWebsite.Models;
+
+namespace PierresWebsite.Controllers
+{
+  public class VendorsController : Controller
+  {
+    
+  }
+}
