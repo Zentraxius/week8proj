ec531355bb3db258484f17e99e90e48074bccda3
Cody Fritz <clanalia55@gmail.com>
2020-07-24 14:45:21 -0700

    save progress towards controller.cs files

--- PierresWebsite/Controllers/OrdersController.cs
+++ PierresWebsite/Controllers/OrdersController.cs
@@ -6,6 +6,11 @@ namespace PierresWebsite.Controllers
 {
   public class OrdersController : Controller
   {
-    
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId);
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
   }
 }
