cebcb27086b4da696c3f31ebabc38bb05d582dc4
Cody Fritz <clanalia55@gmail.com>
2020-07-26 11:02:42 -0700

    finish writing out basic tests, prepare for push to GH

--- PierresWebsite.Tests/ModelsTests/OrdersTests.cs
+++ PierresWebsite.Tests/ModelsTests/OrdersTests.cs
@@ -21,12 +21,82 @@ namespace PierresWebsite.Tests
     [TestMethod]
     public void OrderDescription_DoesOrderHaveDescription_Exists()
     {
-      string description = "Example String";
+      string description = "Wash the Sesame Seeds";
       Order newOrder = new Order(description);
 
       string result = newOrder.Description;
 
       Assert.AreEqual(description, result);
+    }
+    [TestMethod]
+    public void CheckDescription_SetDescription_String()
+    {
+      string description = "Check the flour";
+      Order newOrder = new Order(description);
+
+      string updatedDescription = "Stock the yeast";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> {};
+
+      List<Order> result = Order.GetAll();
+
+      foreach (Order thisOrder in result)
+      {
+        Console.WriteLine("Output from empty list GetAll test: " + thisOrder.Description);
+      }
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      
+      string description01 = "Knead the dough";
+      string description02 = "Preheat the oven";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+     
+      List<Order> result = Order.GetAll();
+
+      foreach (Order thisOrder in result)
+      {
+        Console.WriteLine("Output from second GetAll test: " + thisOrder.Description);
+      }
+
+     
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetId_GetIdForOrder_Int()
+    {
+      string description = "Add a pinch of salt";
+      Order newOrder = new Order(description);
+
+      int result = newOrder.Id;
+
+      Assert.AreEqual(1, result);
+    }
+    [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order()
+    {
+      string description01 = "Wash your hands";
+      string description02 = "Preserve the sourdough starter";
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+
+      Order result = Order.Find(2);
+      Assert.AreEqual(newOrder2, result);
     }
   }
 }
--- PierresWebsite.Tests/ModelsTests/VendorsTests.cs
+++ PierresWebsite.Tests/ModelsTests/VendorsTests.cs
@@ -1,19 +1,87 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresWebsite.Models;
 using System.Collections.Generic;
 using System;
 
 namespace PierresWebsite.Tests
 {
   [TestClass]
-  public class VendorsTests //: IDisposable
+  public class VendorTest : IDisposable
   {
-    // public void Dispose()
-    // {
-    //   Something.ClearAll();
-    // }
-    [TestMethod]
-    public void test_test_test()
+    public void Dispose()
     {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test Vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_TellsName_String()
+    {
+      string name = "Stacy's Bakery";
+      Vendor newVendor = new Vendor(name); 
+
+      string result = newVendor.Name;
+
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorID_Int()
+    {
+      string name = "John's Muffins";
+      Vendor newVendor = new Vendor(name);// ID 3
+      int result = newVendor.Id;
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "Pierre's";
+      string name02 = "Bakery";
+
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+      List<Vendor> result = Vendor.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name01 = "Pierre's";
+      string name02 = "Bakery";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+
+      Vendor result = Vendor.Find(2);
+
+      Assert.AreEqual(newVendor2, result);
+    }
+
+    [TestMethod]
+    public void AddOrder_AssociatesOrderWithVendor_OrderList()
+    {
+      string description = "Behold the Yeast!";
+      Order newOrder = new Order(description);
+      List<Order> newList = new List<Order> { newOrder };
+      string name = "Pierre's!";
+      Vendor newVendor = new Vendor(name);
+      newVendor.AddOrder(newOrder);
+
+      List<Order> result = newVendor.Orders;
+
+      CollectionAssert.AreEqual(newList, result);
 
     }
   }
